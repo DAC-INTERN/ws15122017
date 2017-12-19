1a8f4b8156997874c193070d0351a4de34143c09
Wolf <cgvnd123@gmail.com>
2017-12-19 17:07:58 +0700

    Commit #1 Update choose size and color for user !!!

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,23 @@
 $(document).ready(function () {
 
     $('#submit').click(function () {
+        var size = $('#size').val();
         var text = $('#description').val();
-        var imgSrc = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' + encodeURI(text);
+        var color = $('#ColorPicker').val();
+        if (!text) {
+            alert("Please, Enter your text");
+        } else {
+            if (!size) {
+                alert('You do not enter size, System will choose 300x300 by default');
+                size = '300x300';
+            };
+            color = color.slice(1);
+            var imgSrc = 'https://api.qrserver.com/v1/create-qr-code/?data=' + encodeURI(text);
+            imgSrc += '&size=' + size;
+            imgSrc += '&color=' + color;
+            console.log(imgSrc);
 
-        $('#qrcode').attr('src', imgSrc);
+            $('#qrcode').attr('src', imgSrc);
+        }
     });
-
 });
