22a0e422646370836d3a61fdabcdd8b5a668df12
Unknown <thinh01635492537@gmail.com>
2017-12-22 02:56:29 +0700

    Add some CSS attributes and edit HTML layout

--- js/custom.js
+++ js/custom.js
@@ -3,17 +3,18 @@ $(document).ready(function () {
     $('#submit').click(function () {
         var text = $('#description').val();
         var imgSrc = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' + encodeURI(text);
-        var color = $('#ColorPicker').val();
+        var color = $('#ColorPicker1').val();
+        var backcolor= $('#ColorPicker2').val();
         var size = $('#imgsize').val();
         if (text=="" || color=="" || size=="")
         {
         	alert('You must write your text , pick your size and write out your color');
         }
         imgSrc+='&size='+size;
-        // imgSrc+='&color='+color;
         console.log(imgSrc);
         $('#qrcode').attr('src', imgSrc);
-        $('#qrcode').css('color',color)
+        $('#qrcode').css('color',color);
+        $('#qrcode').css('background-color',backcolor);
     });
 
 });
