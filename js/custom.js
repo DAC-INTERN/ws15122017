79f705ea588c2b7e53204a4bb082c629342c8938
Jared Chu <me@jaredchu.com>
2017-12-22 14:18:04 +0700

    update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,25 @@
 $(document).ready(function () {
 
     $('#submit').click(function () {
+
         var text = $('#description').val();
         var imgSrc = 'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' + encodeURI(text);
         var color = $('#ColorPicker1').val();
         var backcolor= $('#ColorPicker2').val();
         var size = $('#imgsize').val();
+
+        //set color for api
+        imgSrc += '&color=' + color.slice(1);
+        imgSrc += '&bgcolor=' + backcolor.slice(1);
+
         if (text=="" || color=="" || size=="")
         {
         	alert('You must write your text , pick your size and write out your color');
         }
         imgSrc+='&size='+size;
-        console.log(imgSrc);
-        $('#qrcode').attr('src', imgSrc);
-        $('#qrcode').css('color',color);
-        $('#qrcode').css('background-color',backcolor);
+
+        var qrCode = $('#qrcode');
+        qrCode.attr('src', imgSrc);
     });
 
 });
